dba50f1d81693c84390a84754ab6427c7fa7fd19
Darağay Volkan Kavlan <119038018+VolkanKavlan@users.noreply.github.com>
2023-02-01 22:45:10 +0300

    ActivityManager sınıfında methodlar düzenlendi Form6 ve form7 tasarımları kabaca yapıldı(Eksikler var)

--- Diet.UI/Form6.cs
+++ Diet.UI/Form6.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Diet.BLL;
+using Diet.DAL.Entities;
+using MaterialSkin.Controls;
+
 
 namespace Diet.UI
 {
-    public partial class Form6 : Form
+    public partial class Form6 : MaterialForm
     {
+        DietAppContext db = new DietAppContext();
         public Form6()
         {
             InitializeComponent();
+        }
+
+        private void btnAddStepCount_Click(object sender, EventArgs e)
+        {
+            double LostCalorieByStep= (ActivityManager.CalculateConsumedCalorieByStep((int)nmrStepCount.Value));
+            lblKCAL.Text = LostCalorieByStep.ToString() + " kCal TEBRİKLER.";
         }
     }
 }

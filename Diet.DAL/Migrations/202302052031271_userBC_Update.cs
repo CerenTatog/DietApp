d5a40730d7ddfe0e2a9c3d02390893bf69f5177f
Ceren Tatoğlu <119038121+CerenTatog@users.noreply.github.com>
2023-02-05 23:32:26 +0300

    userBC update migration

---
+++ Diet.DAL/Migrations/202302052031271_userBC_Update.cs
@@ -0,0 +1,28 @@
+﻿namespace Diet.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class userBC_Update : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.UserBCs", "Weight", c => c.Double(nullable: false));
+            AddColumn("dbo.UserBCs", "Height", c => c.Double(nullable: false));
+            AddColumn("dbo.UserBCs", "ActivityStatus", c => c.Int(nullable: false));
+            DropColumn("dbo.UserBCs", "BodyCharacteristicType");
+            DropColumn("dbo.UserBCs", "MeasurementValue");
+            DropColumn("dbo.UserBCs", "MeasureType");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.UserBCs", "MeasureType", c => c.Int(nullable: false));
+            AddColumn("dbo.UserBCs", "MeasurementValue", c => c.Double(nullable: false));
+            AddColumn("dbo.UserBCs", "BodyCharacteristicType", c => c.Int(nullable: false));
+            DropColumn("dbo.UserBCs", "ActivityStatus");
+            DropColumn("dbo.UserBCs", "Height");
+            DropColumn("dbo.UserBCs", "Weight");
+        }
+    }
+}
